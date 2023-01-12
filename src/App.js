e41d39193cb9eeddfd3b1a4ed651091b78583ff9
Paul Isoard <paulisoard2911@gmail.com>
2023-01-12 12:02:18 +0100

    MovieDetails

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { MoviesController } from "./contexts/MoviesContext";
 import MoviesList from "./components/MoviesList";
 import NavBar from "./components/NavBar";
 import MovieDetails from "./components/MovieDetails";
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 const App = () => {
   return (
@@ -12,7 +15,9 @@ const App = () => {
       <MoviesController>
         <NavBar />
         {/* <MoviesList /> */}
-        <Link to="/movies">Movies</Link>
+        <button>
+          <Link to="/movies">Movies</Link>
+        </button>
         <Routes>
           <Route path="/movies" element={<MoviesList />} />
           <Route path="/movie/:id" element={<MovieDetails />} />
--- src/components/movieDetails.js
+++ src/components/movieDetails.js
@@ -1,7 +1,9 @@
 import { useParams, Link, useLocation, useNavigate } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { MoviesContext } from "../contexts/MoviesContext";
-import axios from 'axios'
+import axios from "axios";
+import "./MovieDetails.css";
+// import CurrencyFormat from 'react-currency-format';
 
 const Movie = () => {
   // let location = useLocation();
@@ -10,7 +12,7 @@ const Movie = () => {
   let { id } = useParams();
 
   // const [moviesData, setMoviesData] = useContext(MoviesContext);
-  const [movie, setMovie] = useState({})
+  const [movie, setMovie] = useState({});
 
   const fetchMovie = async () => {
     await axios
@@ -18,8 +20,8 @@ const Movie = () => {
         `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_API_KEY_V3}`
       )
       .then((res) => {
-        setMovie(res);
-        console.log(res)
+        setMovie(res.data);
+        console.log(res.data, "moviedetails");
       })
       .catch((err) => console.log(err));
   };
@@ -29,10 +31,51 @@ const Movie = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="page">
+      <h1 className="margin">{movie.title}</h1>
+      <p className="margin">
+        <b>Release in theatre :</b> {movie.release_date}
+      </p>
+      <p className="margin">
+        <b>Profits :</b> {movie.revenue - movie.budget} $
+      </p>
+      {/* <h3>Profits : <CurrencyFormat value={movie.revenue - movie.budget} displayType={'text'} thousandSeparator={true} prefix={'$'} /></h3> */}
+      <h4 className="margin">Genres :</h4>
+      <div className="genres">
+        {movie.genres ? (
+          movie.genres.map((e) => {
+            return (
+              <ul style={{ listStyleType: "none"}}>
+                <li>{e.name}</li>
+              </ul>
+            );
+          })
+        ) : (
+          <p>Unknown</p>
+        )}
+      </div>
+      <h4 className="margin">Produced by :</h4>
+      <div className="genres">
+        {movie.production_companies ? (
+          movie.production_companies.map((e) => {
+            return (
+                <ul style={{ listStyleType: "none" }}>
+                  <li>{e.name}</li>
+                </ul>
+            );
+          })
+        ) : (
+          <p>Unknown</p>
+        )}
+      </div>
+      <h4 className="margin">Synopsis : </h4>
+      {movie.overview ? (
+        <p className="synopsis">{movie.overview}</p>
+      ) : (
+        <p>Unknown</p>
+      )}
     </div>
-  )
+  );
 };
 
 export default Movie;
