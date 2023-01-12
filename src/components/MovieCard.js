302c19c713393a860d0397422ead8904e45914d0
Barbara <barbara@git>
2023-01-12 15:06:45 +0100

    fix for pull request

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -10,7 +10,7 @@ import {Link} from 'react-router-dom'
 import { useNavigate } from "react-router-dom";
 
 export default function MediaCard(props) {
-  const [movies, setMovies] = useContext(MoviesContext);
+  // const [movies, setMovies] = useContext(MoviesContext);
 
   let navigate = useNavigate();
 
